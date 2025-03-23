0afb88c72f946e20d5a9a5f4c5c373ee3f3233a3
Aaron <128830108+aaron-d-e@users.noreply.github.com>
2025-03-23 11:33:10 -0700

    Update proj5-main.cpp

--- Project5/proj5-main.cpp
+++ Project5/proj5-main.cpp
@@ -1,19 +1,73 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "proj5-BUAthleteList.h"
 using namespace std;
 
 int main(){
 
     BUAthleteList list;
+    ifstream NCAAFile;
+    ifstream BUFile;
+    string str;
+    int id;
+    string name;
+    string decision;
+    string school;
+    int size = 0;
+    int searchID;
+    string pos;
+    int eval;
 
-    list.addNCAAAthlete(1234, "AARON", "yes", "Baylor");
-    list.setBUInfo(0, QB, 5);
-    list.addNCAAAthlete(5678, "ERICK", "no", "Baylor");
-    list.setBUInfo(1, OL, 5);
-    list.addNCAAAthlete(5678, "JESSE", "yes", "Utah");
-    list.setBUInfo(2, CB, 3);
+    NCAAFile.open("proj5-NCAAAthlete.csv");
+
+    if (!NCAAFile.is_open()) {
+        cout << "Failed to open NCAAFile." << endl;
+        exit(1);
+    }
+
+    NCAAFile >> id;
+    while (NCAAFile) {
+        NCAAFile.ignore();
+        getline(NCAAFile, name, ',' );
+        getline(NCAAFile, decision, ',');
+        NCAAFile >> school;
+        NCAAFile.ignore();
+        list.addNCAAAthlete(id, name, decision, school);
+        size++;
+
+        NCAAFile >> id;
+    }
+    NCAAFile.close();
+
+    list.sortByID();
+
+    BUFile.open("proj5-BUAthlete.csv");
+    if (!BUFile.is_open()) {
+        cout << "Failed to open BUFile." << endl;
+        exit(1);
+    }
 
 
-     cout << list.toString();
+    while (BUFile >> searchID) {
+        int index;
+        BUFile.ignore();
+        getline(BUFile, pos, ',');
+        BUFile >> eval;
+
+
+        index = list.findById(searchID);
+        if (index != -1) {
+            list.setBUInfo(index, strToPosition(pos) , eval);
+        }
+        else {
+            cout << "Player: " << searchID << "does not exist." << endl;
+        }
+
+    }
+
+
+    cout << list.toString();
+
     return 0;
 }
