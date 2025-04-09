f3a33ab1b6b329a81465e56ddb6205c0c9243013
aaron-d-e <aarone0621@gmail.com>
2025-04-08 20:48:46 -0500

    start of project 8

---
+++ Project8/proj8-bFileSort.cpp
@@ -0,0 +1,18 @@
+/*
+ * file: proj8-bFileSort.cpp
+ * author: Aaron Evans
+ * course: CSI 1440
+ * assignment: project 8
+ * due date: 04/14/2025
+ *
+ * date modified: 04/08/2025
+ * - initial git commit and file setup
+ */ 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	return 0;
+}
